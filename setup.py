6bc0b866c1886d24f290a1f54779e0a4e633cd6d
Vitor Junior <71563179+vitia-fritelle@users.noreply.github.com>
2021-12-30 16:48:32 -0300

    Created setup for Pypi

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = ''.join(["\n",fh.read()])
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A package for getting python code from jupyter notebook.'
+LONG_DESCRIPTION = 'A package for getting python code from jupyter notebook.'
+
+# Setting up
+setup(
+    name="ipynb_converter",
+    version=VERSION,
+    author="Vitia Fritelle (Vitor Guilherme Coutinho de Barros Junior)",
+    author_email="<vitorjunior448@gmail.com>",
+    description=DESCRIPTION,
+    license='MIT',
+    long_description_content_type="text/plain",
+    long_description=long_description,
+    url='https://github.com/vitia-fritelle/ipynb_converter',
+    packages=find_packages(),
+    install_requires=['json', 'typing'],
+    keywords=['python', 'jupyter notebook', 'converter'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
